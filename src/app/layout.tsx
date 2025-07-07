2484f1bfcf9ef42daae79c22eda94182de56bbeb
vitinpicademel <novofaceednv@gmail.com>
2025-07-07 14:45:59 -0300

    Remoção: exclui Meta Pixel (Facebook Pixel) do layout global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,24 +57,6 @@ export default function RootLayout({
             gtag('config', 'G-06NMBZ3BHK');
           `}
         </Script>
-        {/* Meta Pixel */}
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1669907980293154');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-        <noscript>
-          <img height="1" width="1" style={{display:'none'}} src="https://www.facebook.com/tr?id=1669907980293154&ev=PageView&noscript=1" />
-        </noscript>
         {children}
       </body>
     </html>
